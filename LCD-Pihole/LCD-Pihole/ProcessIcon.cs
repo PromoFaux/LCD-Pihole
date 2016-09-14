b3f0f165eea7093f7d2b18bd14a06a3bbe362200
Promofaux <adzjwarner@gmail.com>
2016-09-14 22:35:01 +0100

    Move Pihole stuff out to seperate class

--- LCD-Pihole/LCD-Pihole/ProcessIcon.cs
+++ LCD-Pihole/LCD-Pihole/ProcessIcon.cs
@@ -49,63 +49,7 @@ namespace LCDPlay
             // Attach a context menu.
             ni.ContextMenuStrip = new ContextMenus().Create();
 
-
-            using (WebClient wc = new WebClient())
-            {
-                var json = wc.DownloadString("http://pi.hole/admin/api.php?summaryRaw");
-
-                JObject j = JObject.Parse(json);
-
-                var label = new LCDLabel
-                {
-                    Font = PixelFonts.Small, // The PixelFonts class contains various good fonts for LCD screens.
-                    Text = json,
-                    AutoSize = true,
-                };
-                var tabPage = new LCDTabPage
-                {
-                    Icon = new LCDLabel
-                    {
-                        AutoSize = true,
-                        Text = "A",
-                        Font = PixelFonts.Title
-                    }
-                };
-                tabPage.Controls.Add(label);
-
-
-                var tabPage2 = new LCDTabPage
-                {
-                    Icon = new LCDLabel
-                    {
-                        AutoSize = true,
-                        Text = "B",
-                        Font = PixelFonts.Title
-                    }
-                };
-
-
-                var tabControl = new LCDTabControl();
-                tabControl.TabPages.Add(tabPage);
-                tabControl.TabPages.Add(tabPage2);
-
-                tabControl.SelectedTab = tabPage;
-
-
-
-                // Create an app instance.
-                var app = new LCDApp("Sample App", false, false, false);
-
-                // Add the label control to the app.
-                app.Controls.Add(tabControl);
-                // Make the app the foreground app on the LCD screen.
-                app.PushToForeground();
-
-                // A blocking call. Waits for the LCDApp instance to be disposed. (optional)
-                app.WaitForClose();
-            }
-
-
+            PiHoleApi p = new PiHoleApi();
           
 
         }
